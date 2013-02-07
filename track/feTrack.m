9d4bdf126bb58f4841565129da2231f1d5d9a649
frakkopesto <frakkopesto@gmail.com>
2013-02-06 17:28:17 -0800

    ENH: Added tensor-based tractography

--- track/feTrack.m
+++ track/feTrack.m
@@ -1,4 +1,4 @@
-function feTrack(dt6Dir,fibersFolder,lmax,nSeeds,wmMask)
+function [status, results, fg, pathstr] = feTrack(trackingAlgorithm, dtFile,fibersFolder,lmax,nSeeds,wmMask)
 %
 % This function creates whole-brain white-matter connectome.
 %
@@ -11,18 +11,20 @@ function feTrack(dt6Dir,fibersFolder,lmax,nSeeds,wmMask)
 %
 % Franco (c) Stanford VISTA Team 2012
 
-if notDefined('lmax'),         lmax = [22 24];end
-if notDefined('dt6Dir'),       dt6Dir = '/biac2/wandell6/data/frk/LiFE/data/fp20120420/150dirs_b1000_1';end
-if notDefined('wmMask'),       wmMask = [];end
-if notDefined('fibersFolder'), fibersFolder = 'fe_fibers';end
-if notDefined('nSeeds'),       nSeeds = 200000; end
-
-% Make the full path to the dt6 for and the folder for the fibers
-dtFile  = fullfile(dt6Dir,'dt6.mat');
-fibDir  = fullfile(dt6Dir,fibersFolder);
-
+if notDefined('trackingAlgorithm'), trackingAlgorithm = {'tensor'};end
+if notDefined('dtFile'),            dtFile = ...
+    '/biac2/wandell6/data/frk/LiFE/data/fp20120420/150dirs_b1000_1/dt6.mat';end
+if notDefined('wmMask'),            wmMask  = [];end
+if notDefined('fibersFolder'),      fibersFolder = ...
+    '/azure/scr1/frk/150dirs_b1000_b2000_b4000/life_mrtrix_rep1';end
+if notDefined('lmax'),              lmax    = [8];end
+if notDefined('nSeeds'),            nSeeds  = 500000; end
+  
+runInBackground = 0;
+verbose = 1;
+  
 % make the fibers' folder if it does not exist
-if ~exist(fibDir,'dir'), system(sprintf('mkdir -v %s',fibDir)); end
+if ~exist(fibersFolder,'dir'), system(sprintf('mkdir -v %s',fibersFolder)); end
 
 fprintf('\n\n[%s] Performing whole-brain tractograpy ...\n\n',mfilename);
 
@@ -31,79 +33,49 @@ for il = 1:length(lmax)
   % This first step initializes all the files necessary for mrtrix.
   % This can take a long time.
   files           = mrtrix_init(dtFile, lmax(il),fibersFolder,wmMask);
-  runInBackground = 0;
-  verbose = 1;
+
   
   % MRTRIX - We run this first because it is fast.
-  for ii = [4,5]
+  for ii = 1:length(trackingAlgorithm)
     % Track and save fibers using mrtrix
-    % [status, results, fg, pathstr] = mrtrix_track(csd, roi, mask, mode, nSeeds, bkgrnd, verbose)
-    mrtrix_track(files.csd, files.brainmask, files.wm, switchAlgo(ii), nSeeds, runInBackground, verbose);
+    [status, results, fg, pathstr] = mrtrix_track(files, files.brainmask, files.wm, switchAlgo(trackingAlgorithm{ii}), nSeeds, runInBackground, verbose);
   end
 end
 
-% Track with TEND/FACT
-%
-% Parameters...
-% for performing whole-brain tractography using TEND and FACT
-opts = feFiberTrackOpts;
-for ii = [1,3]
-  % set up file name and options
-  fgWBname = [];
-  fgWBname = fullfile(fibDir,sprintf('fg_whole_brain_%s.mat',switchAlgo(ii)));
-  opts.whichAlgorithm = ii;
-  
-  % track and save the fibers
-  feFiberTrackWholeBrain(dtFile,opts,fgWBname,'both',1,'pdb');
-end
-
+return
 
 %%%%%%%%%%%%%%%%%%%%%
 % Choose algorithm. %
 %%%%%%%%%%%%%%%%%%%%%
 function algo = switchAlgo(algo)
-% Gets the algorithm type for tracktography given the index stored in
-% dtiFiberTraker.m
+% Gets the algorithm type for tracktography.
 %
 % Tracking algorithm: 
-% 0=FACT Euler, 1=FACT RK4, 2=TEND Euler, 3=TEND RK4,
-% 4=prob, 5=stream. (Last two use mrtrix)
+% 
+% 1=prob, 2=stream, 3=tensor.
 %
 % Franco
 
-if isnumeric(algo) % from index to string  
+if isnumeric(algo) % from index to string
   switch algo
-    case {0}
-      algo = 'FACT_euler';
     case {1}
-      algo = 'FACT_RK4';
-    case {2}  
-      algo = 'TEND_euler';
-    case {3} 
-      algo = 'TEND_RK4';
-    case {4}  
       algo = 'prob';
-    case {5} 
+    case {2}
       algo = 'stream';
+    case {3}
+      algo = 'tensor';
     otherwise
       keyboard
   end
   
-elseif ischar(index)
+elseif ischar(algo)
   switch algo
-    case {'FACTe','FACTE','FACT Euler','FACT_euler'}
-      algo = 0;
-    case {'FACT','fact rk4','FACT_RK4','FACT RK4'}
-      algo = 1;
-    case {'TENDe','TENDE','TEND_euler'}
-      algo = 2;
-    case {'TEND','TEND_RK4','TEND RK4', 'tend rk4'}
-      algo = 3;    
-    case {'prob','probabilistic','mrtrix probabilistic'}
-      algo = 4;
+     case {'prob','probabilistic','mrtrix probabilistic'}
+      algo = 'prob';
     case {'stream','streamline','determinstic', 'mrtrix deterministic'}
-      algo = 5;
-
+      algo = 'stream';
+    case {'tensor','tensor based','mrtrix tensor based tractography'}
+      algo = 'tensor';
     otherwise
       keyboard
   end
