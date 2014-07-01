8bc414404442fdac470318476948def2adc3aac4
Franco Pestilli <frakkopesto@gmail.com>
2014-07-01 15:04:34 -0700

    Update feOpenLocalCluster.m

--- utility/feOpenLocalCluster.m
+++ utility/feOpenLocalCluster.m
@@ -1,5 +1,4 @@
 function feOpenLocalCluster
-%
 % Initializes a local matlab cluster if the parallel matlab toolbox is
 % available.
 %
@@ -9,16 +8,24 @@ function feOpenLocalCluster
 
 % Initialize a local matlab cluster to speed up some of the processes.
 if exist('matlabpool','file')
-if (matlabpool('size') == 0), 
-    if exist('parcluster','file')
-    c = parcluster;
-    c.NumWorkers = 12;
-    else
-    matlabpool open;     
-    end
+   try 
+     if (matlabpool('size') == 0), 
+        if exist('parcluster','file')
+           c = parcluster;
+           c.NumWorkers = 12;
+        else
+           matlabpool open;     
+        end
+     else
+        disp('[feOpenLocalCluster] Found Matlab parallel cluster open, not intializing.')
+     end
+   catch
+     disp('[feOpenLocalCluster] Cannot find the Matlab parallel toolbox, not intializing a cluster.')
+     disp('[feOpenLocalCluster] Many computations will be substantially slower, without the parallel toolbox.')
+   end
 else
-    disp('[feOpenLocalCluster] Found Matlab parallel cluster open, not intializing.')
+      disp('[feOpenLocalCluster] Cannot find the Matlab parallel toolbox, not intializing a cluster.')
+      disp('[feOpenLocalCluster] Many computations will be substantially slower, without the parallel toolbox.')
 end
-    disp('[feOpenLocalCluster] Cannot find the Matlab parallel toolbox, not intializing a cluster.')
-    disp('[feOpenLocalCluster] Many computations will be substantially slower, without the parallel toolbox.')
+
 end
