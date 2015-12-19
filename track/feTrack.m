3c1afde1de6294c9f8ac5f9d2d813f35fee4666e
htakemur <htakemur@stanford.edu>
2015-12-19 13:45:14 +0900

    Adding curvature/cutoff parameters in feTrack, and some functions on ET

--- track/feTrack.m
+++ track/feTrack.m
@@ -1,25 +1,40 @@
-function [status, results, fg, pathstr] = feTrack(trackingAlgorithm, dtFile,fibersFolder,lmax,nSeeds,wmMask)
+function [status, results, fg, pathstr] = feTrack(trackingAlgorithm, dtFile,fibersFolder,lmax,nSeeds,wmMask,curvature,cutoff)
 % This function creates whole-brain white-matter connectome.
 %
 % [status, results, fg, pathstr] = feTrack(trackingAlgorithm, dtFile,fibersFolder,[lmax],[nSeeds],[wmMask])
 %
 % Inputs:
+%  - trackingAlgorithm: tracking algorithm to use for tractography (should
+%     be defined in string). e.g. {'tensor'}, {'stream'},{'prob'}.
 %  - dt6Dir - directory containing the dt6.mt file.
 %  - fibersFolder - folder to use to save all the fibers computed.
+%    (default: current directory)
 %  - lmax - Max harmonic order. Default = 10.
 %  - nSeeds - number of seeds to use for mrtrix, this determines the final
 %             number of fibers.
+%  -curvature The minimum radius of curvature required for tractography
+%            (default is 2 for DT_STREAM, 0 for SD_STREAM, 1 for
+%            SD_PROB)
+%  -cutoff The stopping criteria for tractography based on FA (DT_STREAM)
+%           or FOD amplitude cutoff (CSD tracking).  
 %
-%
-% Copyright (2013-2014), Franco Pestilli, Stanford University, pestillifranco@gmail.com.
+% Copyright (2013-2014), Franco Pestilli, Stanford University,
+% 
+% Log:
+% 2013-2014: FP wrote the function
+% 2015 Dec: HT add functionalities for ET project
+% 
+
 if notDefined('trackingAlgorithm'), trackingAlgorithm = {'tensor'};end
-if notDefined('dtFile'),            dtFile = ...
-    '/biac2/wandell6/data/frk/LiFE/data/fp20120420/150dirs_b1000_1/dt6.mat';end
+if notDefined('dtFile'),          
+    error('The location of dt6 file should be specified');end
 if notDefined('wmMask'),            wmMask  = [];end
-if notDefined('fibersFolder'),      fibersFolder = ...
-    '/azure/scr1/frk/150dirs_b1000_b2000_b4000/life_mrtrix_rep1';end
+if notDefined('fibersFolder'),    
+    fibersFolder = pwd;end
 if notDefined('lmax'),              lmax    = [8];end
 if notDefined('nSeeds'),            nSeeds  = 500000; end
+if notDefined('cutoff'),  cutoff = [];end
+if notDefined('curvature'),  curvature = [];end
   
 runInBackground = 0;
 verbose = 1;
@@ -38,7 +53,7 @@ for il = 1:length(lmax)
   % MRTRIX - We run this first because it is fast.
   for ii = 1:length(trackingAlgorithm)
     % Track and save fibers using mrtrix
-    [status, results, fg, pathstr] = mrtrix_track(files, files.brainmask, files.wm, switchAlgo(trackingAlgorithm{ii}), nSeeds, runInBackground, verbose);
+    [status, results, fg, pathstr] = mrtrix_track(files, files.brainmask, files.wm, switchAlgo(trackingAlgorithm{ii}), nSeeds, curvature, cutoff, runInBackground, verbose);
   end
 end
 
